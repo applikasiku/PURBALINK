d02eeb50457a3da92c91fa07c0f51cfd233e2754
applikasiku <amelnewsid@gmail.com>
2026-09-18 11:13:09 +0700

    Bump PWA cache for V2.2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "purbalink-v2-1-midtrans-20260918";
+const CACHE = "purbalink-v2-2-ui-jooble-20260918";
 const ASSETS = [
   "./","./index.html","./purbalink-home.html","./loker.html","./shop.html","./video.html","./profile.html","./login.html","./register.html","./admin-dashboard.html",
   "./tentang.html","./redaksi.html","./pedoman-media-siber.html","./privacy.html","./terms.html","./kontak.html",
