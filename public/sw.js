045c6e71268f9df56ffad33fe4d70bc5295ad60a
applikasiku <amelnewsid@gmail.com>
2026-09-18 15:15:14 +0700

    Bump PWA cache for responsive admin

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "purbalink-v2-5-admin-generator-visible-20260918";
+const CACHE = "purbalink-v2-6-admin-responsive-pwa-20260918";
 const ASSETS = [
   "./","./index.html","./purbalink-home.html","./loker.html","./shop.html","./video.html","./profile.html","./login.html","./register.html","./admin-dashboard.html",
   "./tentang.html","./redaksi.html","./pedoman-media-siber.html","./privacy.html","./terms.html","./kontak.html",
