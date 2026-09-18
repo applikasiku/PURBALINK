62fadd6af770e5c980c47fb93b9a37c95905c823
applikasiku <amelnewsid@gmail.com>
2026-09-19 01:41:06 +0700

    Bump PWA cache for V2.6 ads and admin polish

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "purbalink-v2-8-admin-desktop-fix-20260918";
+const CACHE = "purbalink-v2-6-ads-admin-polish-20260919";
 const ASSETS = [
   "./","./index.html","./purbalink-home.html","./loker.html","./shop.html","./video.html","./profile.html","./login.html","./register.html","./admin-dashboard.html",
   "./tentang.html","./redaksi.html","./pedoman-media-siber.html","./privacy.html","./terms.html","./kontak.html",
