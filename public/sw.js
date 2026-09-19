40f57164e9baa7748b869e651fe8bba5a3fa0a96
applikasiku <amelnewsid@gmail.com>
2026-09-20 06:31:49 +0700

    Refresh PWA cache for V5.5 editorial feed pages

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "purbalink-v5-4-clean-anchor-icons-20260920";
+const CACHE = "purbalink-v5-5-editorial-feeds-20260920";
 const ASSETS = [
   "./","./index.html","./purbalink-home.html","./loker.html","./shop.html","./video.html","./profile.html","./login.html","./register.html","./admin-dashboard.html",
   "./tentang.html","./redaksi.html","./pedoman-media-siber.html","./privacy.html","./terms.html","./kontak.html",
