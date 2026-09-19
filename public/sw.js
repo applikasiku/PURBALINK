79fba7fdce6e73e56a1ea633baa27d1401aa7333
applikasiku <amelnewsid@gmail.com>
2026-09-20 06:48:15 +0700

    Refresh cache for category article feeds

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "purbalink-v5-7-category-icons-20260920";
+const CACHE = "purbalink-v5-8-category-feeds-20260920";
 const ASSETS = [
   "./","./index.html","./purbalink-home.html","./loker.html","./shop.html","./video.html","./profile.html","./login.html","./register.html","./admin-dashboard.html",
   "./tentang.html","./redaksi.html","./pedoman-media-siber.html","./privacy.html","./terms.html","./kontak.html",
