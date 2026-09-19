08a87cd3336a46ace6e3215c8b6018944ba16c37
applikasiku <amelnewsid@gmail.com>
2026-09-20 05:54:51 +0700

    Refresh cache for rebuilt anchor ad

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "purbalink-v5-1-ads-icons-fix-20260920";
+const CACHE = "purbalink-v5-2-anchor-rebuild-20260920";
 const ASSETS = [
   "./","./index.html","./purbalink-home.html","./loker.html","./shop.html","./video.html","./profile.html","./login.html","./register.html","./admin-dashboard.html",
   "./tentang.html","./redaksi.html","./pedoman-media-siber.html","./privacy.html","./terms.html","./kontak.html",
