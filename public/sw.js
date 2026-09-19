54229fbfe7979c666829abc07ed884e467ee68a8
applikasiku <amelnewsid@gmail.com>
2026-09-20 05:39:45 +0700

    Refresh PWA cache for V5.1 homepage and article

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "purbalink-v5-ads-articles-20260920";
+const CACHE = "purbalink-v5-1-home-article-20260920";
 const ASSETS = [
   "./","./index.html","./purbalink-home.html","./loker.html","./shop.html","./video.html","./profile.html","./login.html","./register.html","./admin-dashboard.html",
   "./tentang.html","./redaksi.html","./pedoman-media-siber.html","./privacy.html","./terms.html","./kontak.html",
